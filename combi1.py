3d3adeaa1a0d5b0f31f9ab4f0553a5d05e3d29bc
ApoloJ <mjlozanoh98@gmail.com>
2020-11-09 13:46:49 -0500

    cambios en el menu

--- combi1.py
+++ combi1.py
@@ -1,4 +1,15 @@
-def combi(self, p, k, word):
-    p = self.p
-    k = self.k
-    word = self.word
+class Combi:
+    def __init__(self, word, p, k):
+        self.p = p
+        self.k = k
+        self.word = word
+
+    def split(self, word):
+        return [char for char in word]
+
+    def firstP(self):
+        from itertools import permutations
+
+        perm = permutations(self.split(self.word))
+        for i in list(perm):
+            print(i)
